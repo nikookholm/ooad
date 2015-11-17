77b099189c212d68cba9f200cf808fb868777a68
amal q <zizja4ka@yahoo.dk>
2015-11-17 15:31:36 +0100

    lavet noget label og box til opretReservation

--- src/ooad/views/OpretReservationView.java
+++ src/ooad/views/OpretReservationView.java
@@ -3,50 +3,76 @@ package ooad.views;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class OpretReservationView extends Composite {
 	
-	private VerticalPanel vP = new VerticalPanel();
+	private  VerticalPanel vP ;
 		
-	private Label createReservL = new Label("Opret reservation");
+    private Label createReservL, 
+    			  kundeInfoL,
+    			  customerNameL,
+    			  costumerEmailL ,
+    			  IDcustomerL,
+    			  datoL ,
+    			  startDatoL ,
+    			  endDatoL ,
+    			  antalPersone,
+    			  voksenL,
+    			  børnL,
+    			  hundL
+    			  ;
+    DatePicker df ;
+	private ListBox antlVoksenLB, antalBørnLB;
+	private TextBox nameBox, idBox, emailBox, startBox, endBox;
 	
-	private Label kundeInfoL = new Label ("Information om kunden");
-	private Label customerNameL = new Label ("Navn");
-	private Label costumerEmailL = new Label( "email");
-	private Label IDcustomerL = new Label ("ID");
-	
-	private Label datoL = new Label("reservation dato");
-	private Label startDatoL = new Label("Start");
-	private Label endDatoL = new Label ("slut");
-		
-	private TextBox nameBox = new TextBox();
-	private TextBox idBox = new TextBox();
-	private TextBox emailBox = new TextBox();
-	
-	private TextBox startBox = new TextBox();
-	private TextBox endBox = new TextBox();
-	
-	FlexTable ft; 
+	private FlexTable ft;
 	
 	public OpretReservationView (){
-			
 		
-		 
-		vP.add(createReservL);
+		vP = new VerticalPanel(); 
 		
+		ft = new FlexTable();
+		
+		df = new DatePicker();
+		
+		antlVoksenLB = new ListBox();
+		antalBørnLB = new ListBox();
+		
+		createReservL = new Label("Opret reservation");
+		kundeInfoL = new Label ("Information om kunden");
+		customerNameL = new Label ("Navn");
+		costumerEmailL = new Label( "email");
+//		IDcustomerL = new Label ("ID");
+		datoL = new Label("reservation dato");
+		startDatoL = new Label("Start");
+		endDatoL = new Label ("slut");
+		antalPersone = new Label("antal personer:");
+		voksenL = new Label("Voksen");
+		børnL = new Label(" Børn");
+		hundL = new Label("Pet");
+		
+		nameBox = new TextBox();
+//		idBox = new TextBox();
+		emailBox = new TextBox();
+		startBox = new TextBox();
+		endBox = new TextBox();
+		
+	
 		createReservL.setStyleName("caption");
 		
 		ft.setWidget(0, 0, kundeInfoL);
 		
-		ft.setWidget(1, 0, IDcustomerL);
-		ft.setWidget(1, 1, customerNameL);
-		ft.setWidget(1, 2, costumerEmailL);
+//		ft.setWidget(1, 0, IDcustomerL);
+		ft.setWidget(1, 0, customerNameL);
+		ft.setWidget(1, 1, costumerEmailL);
 		
-		ft.setWidget(2, 0, idBox);
-		ft.setWidget(2, 1, nameBox);
-		ft.setWidget(2, 2, emailBox);
+//		ft.setWidget(2, 0, idBox);
+		ft.setWidget(2, 0, nameBox);
+		ft.setWidget(2, 1, emailBox);
 		
 		ft.setWidget(3, 0, datoL);
 		
@@ -55,8 +81,14 @@ public class OpretReservationView extends Composite {
 		
 		ft.setWidget(5, 0, startBox);
 		ft.setWidget(5, 1, endBox);
+		ft.setWidget(5, 2, df);
 		
+		
+		
+		
+		vP.add(createReservL);
 		vP.add(ft);
+		
 		initWidget(vP);
 	
 		
