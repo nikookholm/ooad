47333be6bb35cdd58303f3f404fec669b87d5266
amal q <zizja4ka@yahoo.dk>
2015-11-19 08:37:47 +0100

    button Ok og Cancel mangler ClickHandler. i cancelBtn ved ikke hvordan lavers new view:( har idea om hvordan skal laves info view til kunde. mangler tid):

--- src/ooad/views/OpretReservationView.java
+++ src/ooad/views/OpretReservationView.java
@@ -1,17 +1,23 @@
 package ooad.views;
 
+import ooad.client.Ooad;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Text;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.datepicker.client.DatePicker;
-import com.ibm.icu.util.Calendar;
+import com.sun.java_cup.internal.runtime.Scanner;
 
 public class OpretReservationView extends Composite {
 	
@@ -19,34 +25,25 @@ public class OpretReservationView extends Composite {
 	private HorizontalPanel hP;
 	
 		
-    private Label createReservL, 
-    			  kundeInfoL,
-    			  customerNameL,
-    			  costumerEmailL ,
-    			  IDcustomerL,
-    			  datoL ,
-    			  startDatoL ,
-    			  endDatoL ,
-    			  antalPersone,
-    			  voksenL,
-    			  børnL,
-    			  hundL
-    			  ;
-    private  DatePicker df ;
+    private Label createReservL,kundeInfoL, customerNameL, costumerEmailL, IDcustomerL, datoL, startDatoL, endDatoL,antalPersone,voksenL,børnL,hundL;
+    private  DatePicker df, df2, calendar ;
     private CheckBox cb;
     private ListBox antlVoksenLB, antalBørnLB;
 	private TextBox nameBox, idBox, emailBox, startBox, endBox, voksBox, børnBox;
-	private Button voksB, børnB;
+	private Button voksB, børnB, okBtn, cancelBtn;
+	private PopupPanel pup;
+	private Text info;
 	
-	
+	private Scanner scn;
+
 	private FlexTable ft, ft2;
+	
+	private Ooad ooad;
 	
 	
 	public OpretReservationView (){
 		
 		hP = new HorizontalPanel();
-	
-	
 		vP = new VerticalPanel(); 
 		vP2 = new VerticalPanel();
 		
@@ -54,12 +51,16 @@ public class OpretReservationView extends Composite {
 		ft2 = new FlexTable();
 		cb = new CheckBox();
 		df = new DatePicker();
-
+		df2 = new DatePicker();
+		calendar = new DatePicker();
+		
+		Image icon = new Image();
+		pup = new PopupPanel();
 		
 		antlVoksenLB = new ListBox();
 		antalBørnLB = new ListBox();
-		
-	
+		okBtn = new Button("OK");
+		cancelBtn = new Button("Annuller");
 		
 		createReservL = new Label("Opret reservation");
 		kundeInfoL = new Label ("Information om kunden");
@@ -74,17 +75,24 @@ public class OpretReservationView extends Composite {
 		børnL = new Label(" Børn");
 		hundL = new Label("Pet");
 		
-		nameBox = new TextBox();
+		nameBox  = new TextBox();
 //		idBox = new TextBox();
 		emailBox = new TextBox();
 		startBox = new TextBox();
-		endBox = new TextBox();
-		voksBox = new TextBox();
-		børnBox = new TextBox();
+		endBox   = new TextBox();
+		voksBox  = new TextBox();
+		børnBox  = new TextBox();
 		
-		
-	
+		for ( int i =0 ;  i <=10;  i++){
+			
+			antlVoksenLB.addItem("" + i);
+			antalBørnLB.addItem("" + i);
+			
+		}
+
 		createReservL.setStyleName("caption");
+	
+		
 		
 		ft.setWidget(0, 0, kundeInfoL);
 		
@@ -103,21 +111,51 @@ public class OpretReservationView extends Composite {
 		
 		ft.setWidget(5, 0, startBox);
 		ft.setWidget(5, 1, endBox);
-//		ft.setWidget(5, 2, df);
 		
-		ft.setWidget(6, 0, antalPersone);
+		ft.setWidget(6, 0, df);
 		
-		ft.setWidget(7, 0, voksenL);
-		ft.setWidget(7, 1, børnL);
 	
-		ft.setWidget(8, 0, antlVoksenLB);
-		ft.setWidget(8, 1, antalBørnLB);
+		ft.setWidget(6, 1, df2);
 		
-		ft.setWidget(9, 0, hundL);
-		ft.setWidget(9, 1, cb);
 		
-		ft2.setWidget(0, 6, df);
+		ft.setWidget(7, 0, antalPersone);
+		antalPersone.setStyleName("input-text");
+		
+		ft.setWidget(8, 0, voksenL);
+		ft.setWidget(8, 1, børnL);
 	
+		ft.setWidget(9, 0, antlVoksenLB);
+		ft.setWidget(9, 1, antalBørnLB);
+		
+		ft.setWidget(10, 0, hundL);
+		ft.setWidget(10, 1, cb);
+		
+//		ft2.setWidget(0, 6, df);
+		
+		ft.setWidget(14, 0, cancelBtn);
+		cancelBtn.setEnabled(true);
+		cancelBtn.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				ooad.onModuleLoad();
+			}
+		});
+		
+		ft.setWidget(14, 1, okBtn);
+		okBtn.setEnabled(false);
+		okBtn.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				// her skal være en funktion som gammer alle kunde info og ønsker reservatione date i oadReservation
+				
+				
+			}
+		});
+		
+		
 		vP.add(createReservL);
 		vP.add(ft);
 		
