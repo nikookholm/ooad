ef75ee3757954cd7ff639865485f579e77eb0c10
Niko <nikooftheworld@gmail.com>
2015-12-03 09:41:29 +0100

    Ændret login information

--- src/ooad/database/Constant.java
+++ src/ooad/database/Constant.java
@@ -6,10 +6,10 @@ package ooad.database;
 public abstract class Constant
 {
 	public static final String
-		server					= "localhost",  // database-serveren
-		database				=  "costakalundborg",     //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "root",     // dit brugernavn = dit studienummer 
-		password				= "";        // dit password som du har valgt til din database
+		server					= "mysql20.cliche.dk",  // database-serveren
+		database				=  "minetality_net",     //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "minetality.net",     // dit brugernavn = dit studienummer 
+		password				= "8EeEahI1";        // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
